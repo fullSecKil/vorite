07385d08d22ca3a5782fb7a94e74308771e3dd54
Dusk <33350026+fullSecKil@users.noreply.github.com>
2020-05-09 14:57:08 +0800

    界面上一些更新

--- src/test/java/com/zui/vorite/VoriteApplicationTests.java
+++ src/test/java/com/zui/vorite/VoriteApplicationTests.java
@@ -1,25 +1,39 @@
 package com.zui.vorite;
 
+import com.zui.vorite.controller.VisitController;
 import com.zui.vorite.dao.CaricatureMapper;
-import com.zui.vorite.pojo.OperationLog;
+import com.zui.vorite.pojo.Caricature;
+import com.zui.vorite.service.CaricatureService;
 import com.zui.vorite.service.GenreCaricatureService;
 import com.zui.vorite.service.OperationLogService;
 import com.zui.vorite.tools.ExtractZip;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+// 指定webEnvironment属性，自动初始化Filter、Servlet
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 public class VoriteApplicationTests {
 
     final static private Logger log = LoggerFactory.getLogger(VoriteApplicationTests.class);
@@ -32,6 +46,37 @@ public class VoriteApplicationTests {
 
     @Autowired
     OperationLogService operationLogService;
+
+    private CaricatureService caricatureService;
+
+    @Autowired
+    void setCaricatureService(CaricatureService caricatureService) {
+        this.caricatureService = caricatureService;
+    }
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void testManageController() throws Exception {
+        RequestBuilder request = null;
+
+        List<Caricature> caricatures = caricatureService.selectAll();
+
+        request = get("/visit/group");
+        mvc.perform(request)
+                .andExpect(status().isOk())
+                // .andExpect(content().string(equalTo(caricatures.toString())))
+                .andDo(print());
+    }
+
+    @Test
+    public void testCaricatureService() {
+        List<Caricature> c = caricatureService.selectAll();
+        Optional<Caricature> caricature = c.stream().filter(caricature1 -> "一拳超人".equals(caricature1.getName())).findFirst();
+        Assert.assertTrue(caricature.isPresent());
+        Assert.assertEquals(caricature.map(caricature1 -> caricature1.getAuthor()).orElse("未知作者"), "村田雄介 ");
+    }
 
     @Test
     public void contextLoads() {
@@ -49,7 +94,7 @@ public class VoriteApplicationTests {
 
     @Test
     public void unZIpTest(){
-        Map<String, String> fileMap = new HashMap<>();
+        Map<Sstring, String> fileMap = new HashMap<>();
         fileMap.put("E:\\download\\caricature\\upload\\一拳超人\\closing.zip", "E:\\download\\caricature\\upload\\一拳超人");
         fileMap.put("E:\\download\\caricature\\upload\\一拳超人\\特别篇.zip", "E:\\download\\caricature\\upload\\一拳超人");
 
